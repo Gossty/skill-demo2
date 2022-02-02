55831a52604a74d3b127ffdb1a99bf02c85b86db
Gosty <91396884+Gossty@users.noreply.github.com>
2022-02-02 04:29:43 +0300

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.additionTwo(12, 123));
+        assertEquals(2, SkillDemo.additionTwo(1, 1));
     }
 }
